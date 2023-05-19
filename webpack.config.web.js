f726c46a9f51b7faa9e301f2dadc74da6e2fb385
Deep Chakraborty <chakraborty.deep013@gmail.com>
2023-05-19 13:02:01 +0530

    Initial Release

--- webpack.config.web.js
+++ webpack.config.web.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production', // Mode (development | production)
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'bundle.web.js', // Output filename
+    filename: 'bard-api.min.js', // Output filename
     libraryTarget: 'umd', // Universal Module Definition (UMD) format
     globalObject: 'this', // Set the global object for the UMD target
   },
